b7c7a6545d4ee0519b0ee935a342e0f3e5d620a5
Chris Hand <cj.hand@gmail.com>
2022-08-29 23:35:41 -0700

    Add ability to add a pin

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -23,13 +23,32 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc
+    func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        print("touchpoint")
+        if gestureRecognizer.state == .began {
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            let touchCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchCoordinates
+            annotation.title = "Annotation Title"
+            annotation.subtitle = "Travel Book"
+            self.mapView.addAnnotation(annotation)
+        }
+    
+        
     }
 
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
     }
